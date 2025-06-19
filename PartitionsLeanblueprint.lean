c00061c96d989eb3e6e269553603c06a8883a5fd
Carter Hastings <carterchastings@gmail.com>
2025-06-18 21:01:20 -0600

    imported the right files to the places

--- PartitionsLeanblueprint.lean
+++ PartitionsLeanblueprint.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `PartitionsLeanblueprint` library.
 -- Import modules here that should be built as part of the library.
 import PartitionsLeanblueprint.Basic
+
+import PartitionsLeanblueprint.IVT
--- convergesTo.lean
+++
@@ -1,5 +0,0 @@
-import Mathlib.Data.Real.Basic
-
-
-def convergesTo (a : ℕ → ℝ) (L : ℝ) :=
-    ∀ ε > 0, ∃ N : ℕ, ∀ n ≥ N, |a n - L| < ε
