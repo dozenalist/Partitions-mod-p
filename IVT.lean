118a7404c8d8aa6d88b91a51bcb971890d9b33d0
Carter Hastings <carterchastings@gmail.com>
2025-06-18 20:13:25 -0600

    set up blueprint and added some code

---
+++ IVT.lean
@@ -0,0 +1,8 @@
+import Mathlib.Data.Real.Basic
+
+
+def convergesTo (a : ℕ → ℝ) (L : ℝ) :=
+    ∀ ε > 0, ∃ N : ℕ, ∀ n ≥ N, |a n - L| < ε
+
+def converges (a : ℕ → ℝ) :=
+    ∃ L : ℝ, convergesTo a L
