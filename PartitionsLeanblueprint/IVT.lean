6ac30326a5eb169a80ae27360b3abba7ce9584f3
Carter Hastings <carterchastings@gmail.com>
2025-06-19 10:09:32 -0600

    added some more theorems to lean and latex

--- PartitionsLeanblueprint/IVT.lean
+++ PartitionsLeanblueprint/IVT.lean
@@ -6,3 +6,19 @@ def convergesTo (a : ℕ → ℝ) (L : ℝ) :=
 
 def converges (a : ℕ → ℝ) :=
     ∃ L : ℝ, convergesTo a L
+
+section theorems
+
+variable {a b : ℕ → ℝ}
+variable {L K c : ℝ}
+
+theorem convergesTo_scalar_mul (h: convergesTo a L) :
+convergesTo (fun n ↦ c * a n) (c * L) := by
+    sorry
+
+theorem convergesTo_add (ha : convergesTo a L) (hb : convergesTo b K) :
+convergesTo (fun n ↦ a n + b n) (L + K) := by
+    sorry
+
+theorem test (ha : convergesTo a L) (hb : convergesTo b K) :
+convergesTo (fun n ↦ a n + b n) (L + K) := by sorry
