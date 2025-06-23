298d6f2dad5383eaa72d14d07220e4f360d5a7be
Carter Hastings <carterchastings@gmail.com>
2025-06-23 13:14:09 -0600

    added a proof of scalar convTo

--- PartitionsLeanblueprint/IVT.lean
+++ PartitionsLeanblueprint/IVT.lean
@@ -14,11 +14,22 @@ variable {L K c : ℝ}
 
 theorem convergesTo_scalar_mul (h: convergesTo a L) :
 convergesTo (fun n ↦ c * a n) (c * L) := by
-    sorry
+    intro ε εpos
+    by_cases hc : c = 0
+    use 0; intro n hn; rw[hc]; simp; exact εpos
+    obtain ⟨N, hN⟩ := h (ε / |c|) (by apply div_pos εpos (abs_pos.2 hc))
+    use N; intro n hn; dsimp
+    calc
+        |c * a n - c * L| = |c| * |a n - L| := by rw[← mul_sub, abs_mul]
+        _ < |c| * (ε / |c|) :=
+            mul_lt_mul' (le_refl _) (hN n hn) (by apply abs_nonneg) (abs_pos.2 hc)
+        _ = ε := by field_simp
+
 
 theorem convergesTo_add (ha : convergesTo a L) (hb : convergesTo b K) :
 convergesTo (fun n ↦ a n + b n) (L + K) := by
     sorry
 
-theorem test (ha : convergesTo a L) (hb : convergesTo b K) :
-convergesTo (fun n ↦ a n + b n) (L + K) := by sorry
+
+
+#check le_max_of_le_left
